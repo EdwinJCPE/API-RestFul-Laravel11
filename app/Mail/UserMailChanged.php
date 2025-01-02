7487244883e60ab9f307839c176bc2f741a448c8
Edwin <edwinjuarez24x@gmail.com>
2025-01-02 22:31:20 +0000

    Implementando el Mailable de UserMailChanged

--- app/Mail/UserMailChanged.php
+++ app/Mail/UserMailChanged.php
@@ -2,23 +2,27 @@
 
 namespace App\Mail;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class UserMailChanged extends Mailable
 {
     use Queueable, SerializesModels;
+    // public $user;
 
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    // public function __construct()
+    // public function __construct(User $user)
+    public function __construct(public User $user)
     {
-        //
+        // $this->user = $user;
     }
 
     /**
@@ -27,7 +31,8 @@ class UserMailChanged extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'User Mail Changed',
+            // subject: 'User Mail Changed',
+            subject: 'Por favor confirma tu nuevo correo.',
         );
     }
 
@@ -37,7 +42,8 @@ class UserMailChanged extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            // view: 'view.name',
+            text: 'emails.confirm',
         );
     }
 
