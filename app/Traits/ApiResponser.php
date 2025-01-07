70711ca93ad7dd00051340123ea3dc4c9dc75bb6
Edwin <edwinjuarez24x@gmail.com>
2025-01-07 22:16:15 +0000

    Retornando las nuevas respuestas transformadas

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 trait ApiResponser
 {
@@ -20,16 +21,54 @@ trait ApiResponser
 
     protected function showAll(Collection $collection, $code = 200)
     {
-        return $this->successResponse(['data' => $collection], $code);
+        // return $this->successResponse(['data' => $collection], $code);
+        // 
+        if ($collection->isEmpty()) {
+            return $this->successResponse(['data' => $collection], $code);
+        }
+
+        // $transformer = $collection->first()->transformer;
+        $transformer = $this->getTransformer($collection->first());
+
+        $collection = $this->transformData($collection, $transformer);
+
+        // return $this->successResponse(['data' => $collection], $code);
+        return $this->successResponse($collection, $code);
+
     } 
 
     protected function showOne(Model $instance, $code = 200)
     {
-        return $this->successResponse(['data' => $instance], $code);
+        // return $this->successResponse(['data' => $instance], $code);
+        // 
+        // $transformer = $instance->transformer;
+        $transformer = $this->getTransformer($instance);
+        $instance = $this->transformData($instance, $transformer);
+
+        // return $this->successResponse(['data' => $instance], $code);
+        return $this->successResponse($instance, $code);
     }
 
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function transformData($data, $transformer)
+    {
+        $transformation = fractal($data, new $transformer);
+
+        return $transformation->toArray();
+    }
+
+    private function getTransformer($model)
+    {
+        $transformer = $model->transformer ?? null;
+
+        if (!$transformer) {
+            throw new HttpException(500, 'No se puedo determinar el transformador.');
+        }
+
+        return $transformer;
     }
 }
