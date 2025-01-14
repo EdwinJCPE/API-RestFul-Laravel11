755b89fed40c398d1e48303ef8fd6ca87d6c0930
Edwin <edwinjuarez24x@gmail.com>
2025-01-14 01:17:36 +0000

    Revert "Mejorando el middleware para resolver el segundo problema"
    This reverts commit e0cdeb34ca7b20d0f7b37c189dc1524613c7ad08.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+<<<<<<< HEAD
 // Auth::routes(); // Illuminate\Support\Facades\Auth;
 
 // Login Routes...
@@ -37,6 +38,9 @@ Route::post('password/confirm', 'App\Http\Controllers\Auth\ConfirmPasswordContro
 Route::get('email/verify', 'App\Http\Controllers\Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'App\Http\Controllers\Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'App\Http\Controllers\Auth\VerificationController@resend')->name('verification.resend');
+=======
+Auth::routes(); // Illuminate\Support\Facades\Auth;
+>>>>>>> parent of e0cdeb3 (Mejorando el middleware para resolver el segundo problema)
 
 Auth::routes();
 
