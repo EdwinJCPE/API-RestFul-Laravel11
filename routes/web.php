da731378b68b7baa72ae7ac8b719165da0bd986a
Edwin <edwinjuarez24x@gmail.com>
2025-01-14 01:16:26 +0000

    Revert "Mejorando el middleware para resolver el segundo problema"
    This reverts commit b68463d34900a5fe3351c0cca8be844bd60b5b21.

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::post('logout', 'App\Http\Controllers\Auth\LoginController@logout')->name(
 // Route::get('register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
 // Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
 
+// Registration Routes...
+Route::get('register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
+
 // Password Reset Routes...
 Route::get('password/reset', 'App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
@@ -33,9 +37,8 @@ Route::post('password/confirm', 'App\Http\Controllers\Auth\ConfirmPasswordContro
 Route::get('email/verify', 'App\Http\Controllers\Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'App\Http\Controllers\Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'App\Http\Controllers\Auth\VerificationController@resend')->name('verification.resend');
-=======
+
 Auth::routes();
->>>>>>> parent of 864b6f8 (Creando la estructura adicional para autenticación)
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
