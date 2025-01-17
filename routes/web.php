2aee069ce4b72edc3510a60cd82af2bfb54672c0
Edwin <edwinjuarez24x@gmail.com>
2025-01-17 23:47:14 +0000

    Preparando la Api para usar los componentes de Passport - Update web.php

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,3 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/', function () {
     return view('welcome');
 })->middleware('guest');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
