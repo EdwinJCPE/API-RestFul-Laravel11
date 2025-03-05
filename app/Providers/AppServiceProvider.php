5e3431e62d3382e39846e9b3284eb127d54922fd
Edwin Juarez <edwinjuarez24x@gmail.com>
2025-03-05 21:06:10 +0000

    Registro de algunos Scopes requeridos para la API

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,6 +40,13 @@ class AppServiceProvider extends ServiceProvider
         Passport::enablePasswordGrant(); // Habilitar el Password Grant
         Passport::enableImplicitGrant();
 
+        Passport::tokensCan([
+            'purchase-product' => 'Crear transacciones para comprar productos determinados',
+            'manage-product' => 'Crear, ver, actualizar y eliminar productos',
+            'manage-account' => 'Obtener la informacion de la cuenta, nombre, email, estado (sin contraseña), modificar datos como email, nombre y contraseña. No puede eliminar la cuenta',
+            'read-general' => 'Obtener información general, categorías donde se compra y se vende, productos vendidos o comprados, transacciones, compras y ventas',
+        ]);
+
         // $this->configureRateLimiting();
         //
 
